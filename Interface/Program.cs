5a4677fbf0c953dfa690c7ad2ab2409523cec8c6
Demetri Warren <dwarren@tql.com>
2024-06-18 11:19:18 -0400

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           IPrint Acct = new Account(); // debug not available bc its defined as an IPrint var. Must be inside of the interface or you cannot reference the data. 
+                                        // if IPrint was changed to be defined as an Account var, debug would work
+
+            Acct.Print("ABC");
+            Acct.Debug();
+        }
+    }
+    public interface IPrint
+    {
+        void Print(string str);
+    }
+    public class Account : IPrint
+    {
+        public void Print(string str)
+        {
+
+        }
+
+        public void Debug()
+        {
+
+        }
+    }
+}
